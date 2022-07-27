3b00267417a9875fdc8515b08e724577ba0f49d0
lalit-03 <rlalitchandra99@gmail.com>
2022-07-27 22:22:31 +0530

    addition operation

--- calculator.c
+++ calculator.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    
+    printf("This is a calculator.\n");
+    int a, b, choice, ans;
+
+    printf("Enter first number(a): ");
+    scanf("%d", &a);
+    printf("Enter second number(b): ");
+    scanf("%d", &b);
+    printf("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division: ");
+    scanf("%d", &choice);
+
+    if(choice==1){
+        printf("a + b = %d", a+b);
+    }
     return 0;
 }
