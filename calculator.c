3b29771c3da2f39cd6df45cc72bc69966e547162
lalit-03 <rlalitchandra99@gmail.com>
2022-07-27 22:32:13 +0530

    Changed int to double, Calculator done

--- calculator.c
+++ calculator.c
@@ -2,30 +2,30 @@
 
 int main(){
     printf("This is a calculator.\n");
-    int a, b, choice, ans;
-
+    double a, b, ans;
+    int choice;
     printf("Enter first number(a): ");
-    scanf("%d", &a);
+    scanf("%lf", &a);
     printf("Enter second number(b): ");
-    scanf("%d", &b);
+    scanf("%lf", &b);
     printf("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division: ");
     scanf("%d", &choice);
 
     if(choice==1){
-        printf("a + b = %d", a+b);
+        printf("a + b = %lf", a+b);
     }
     else if(choice==2){
-        printf("a - b = %d", a-b);
+        printf("a - b = %lf", a-b);
     }
     else if(choice==3){
-        printf("a * b = %d", a*b);
+        printf("a * b = %lf", a*b);
     }
     else if(choice==4){
         if(b==0){
             printf("Division by zero is not possible.");
         }
         else{
-            printf("a / b = %d", a/b);
+            printf("a / b = %lf", a/b);
         }
     }
     else{
